533f441f744cb743916e2b6c6ce37ca71846d6af
Tushar Anand <tusharranand@gmail.com>
2022-03-07 18:57:28 +0530

    taking input for two line and checking if they are equal

--- line_comparison_problem/Program.cs
+++ line_comparison_problem/Program.cs
@@ -13,8 +13,9 @@ namespace line_comparison_probelm
 
         public static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome to Line Comparison Computation Program.");
+           
+            Console.WriteLine("For the first line,");
 
             int x1 = Enter("x1");
             int y1 = Enter("y1");
@@ -23,8 +24,29 @@ namespace line_comparison_probelm
 
             Console.WriteLine("Coordinates for the given line are: ");
             Console.WriteLine("({0},{1}) and ({2},{3})", x1, y1, x2, y2);
-            float len = (float)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Length of given line = {0} units", len);
+            float len_first = (float)Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of given line = {0} units", len_first);
+
+            Console.WriteLine("\nFor the second line,");
+
+            int x3 = Enter("x1");
+            int y3 = Enter("y1");
+            int x4 = Enter("x2");
+            int y4 = Enter("y2");
+
+            Console.WriteLine("Coordinates for the given line are: ");
+            Console.WriteLine("({0},{1}) and ({2},{3})", x3, y3, x4, y4);
+            float len_sec = (float)Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Length of given line = {0} units\n", len_sec);
+
+            if (len_sec == len_first)
+            {
+                Console.WriteLine("Both lines are equal in length.");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equal in length.");
+            }
         }
     }
 }
